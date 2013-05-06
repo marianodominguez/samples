02933bd602b3a251e715f0e7a95ae10ad5f22bb3
Mariano Dominguez <mariano.dm@gmail.com>
2013-05-05 17:42:04 -0700

    removing express tutorial

--- javascript/hello-express/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/hello.txt', function(req, res){ngth);
-  res.send('Hello Express');
-});
-
-app.listen(3000);
-console.log('listening on port 3000');
