a1138d7fbc4a0156974e03afb1ce533d32e0e90f
marianodominguez <mariano.dm@gmail.com>
2013-10-24 09:35:03 -0700

    add palindrome example

---
+++ python/palindrome.py
@@ -0,0 +1,45 @@
+#palindrome conjecture
+
+#given an input, try to construct the palindrome by adding the inverse
+
+#196 does not stop
+#295 neither
+#394
+
+#ex. with 341 341+143=484
+
+def isPalindrome(n):
+    digits = str(n)
+    for i in xrange(len(digits)/2):
+        digit = digits[i]
+        mirror = digits[-(i+1)]
+        if digit != mirror: return False
+    return True
+
+def construct(n):
+    mirror=''
+    digits = str(n)
+    mirror = digits[::-1]
+
+    result = int(digits) + int(mirror)
+    #print digits, "+", mirror, " = ", result
+    return result
+
+#inputString = input("give a number: ")
+#n = int(inputString)
+
+MAX_ITER=2000
+
+for n in xrange(1,1000):
+    iter = 0
+    x = n
+    while not isPalindrome(x):
+        x = construct(x)
+        iter +=1
+        if iter > MAX_ITER:
+            print "iter(", n ,")= unknown"
+            iter = -1
+            break
+    if iter >= 1:
+        print "iter(", n ,")=", iter, " x=", x 
+
