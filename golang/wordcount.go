e39e100827a48646b24c2c0c2513eb1b916ec6a3
marianodominguez <mariano.dm@gmail.com>
2013-12-23 21:20:10 -0800

    add go wordcount exapmple

---
+++ golang/wordcount.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "code.google.com/p/go-tour/wc"
+    "strings"
+)
+
+func WordCount(s string) map[string]int {
+    
+    mapcount := make(map[string]int)
+    
+    for _,word:=range strings.Fields(s) {
+        times, present := mapcount[word]
+        if present {
+        	mapcount[word] = times + 1
+        } else {
+        	mapcount[word] = 1
+        }
+    }
+    
+    return mapcount
+}
+
+func main() {
+    wc.Test(WordCount)
+}
