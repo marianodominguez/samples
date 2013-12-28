ab8a949c3dad3afb58d4073a0dceaf3ee34ddadf
marianodominguez <mariano.dm@gmail.com>
2013-12-28 13:13:31 -0800

    add complex numbers example

---
+++ golang/complex.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "math/cmplx"   
+       )
+
+func Cbrt(x complex128) complex128 {
+    z:= complex(1.0, 0)
+    for i:=0; i<100000; i++ {
+        z -= (z*z*z - x)/(3*x*x)
+    }
+    
+    return z
+}
+
+func main() {
+    fmt.Println( Cbrt(2) )
+    fmt.Println( cmplx.Pow(2.0, 0.33 ) )
+}
