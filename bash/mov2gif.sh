7e698e234678ea6ddd4b8e97e5e0c60ed57382a4
Mariano Dominguez <mariano_molina@apple.com>
2016-01-29 15:19:04 -0800

    remove echo debug

--- bash/mov2gif.sh
+++ bash/mov2gif.sh
@@ -8,9 +8,9 @@ echo $DST
 
 mkdir /tmp/gifs
 mkdir /tmp/pngs
-echo ffmpeg -i ${SRC} -r 10 /tmp/pngs/out%04d.png
-echo sips -s format gif /tmp/pngs/*.png --out /tmp/gifs
-echo gifsicle *.gif --optimize=3 --colors 256 --delay=10 --loopcount --resize 640x480  ${DST}
+ffmpeg -i ${SRC} -r 10 /tmp/pngs/out%04d.png
+sips -s format gif /tmp/pngs/*.png --out /tmp/gifs
+gifsicle *.gif --optimize=3 --colors 256 --delay=10 --loopcount --resize 640x480  ${DST}
 rm -rf /tmp/gifs
 rm -rf /tmp/pngs
 
