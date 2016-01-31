96ba434678346f0e8fc2e1a5d020e6bfb5a37ab1
marianodominguez <mariano.dm@gmail.com>
2016-01-31 10:59:37 -0800

    turtle samples

--- python/tr_polygons.py
+++ python/tr_polygons.py
@@ -5,6 +5,7 @@ backgroundcolor = "#18546A"
 window = turtle.Screen()
 t = turtle.Turtle()
 window.bgcolor(backgroundcolor)
+t.pensize(2)
 
 for i in range(4):
 	t.fd(100)
--- python/tr_stars2.py
+++ python/tr_stars2.py
@@ -6,28 +6,33 @@ backgroundcolor = "#18546A"
 
 window = turtle.Screen()
 window.bgcolor(backgroundcolor)
+window.delay(0)
 
 t = turtle.Turtle()
-t.pensize(2)
 
 def star(n):
-  angle=720/n
-  if n%2==0: angle=180-360/n
-  for i in range(n):
+  angle=180-360/n
+  for i in range(n + n%2):
     t.fd(100)
     t.rt(angle)
 
 t.pu()
+t.fd(200)
 t.rt(90)
 t.fd(200)
 t.lt(90)
 t.pd()
+t.ht()
 
-for i in range(10):
-  star(i+5)
+for i in range(20):
+  star(i+8)
   t.pu()
   t.bk(100)
   t.rt(36)
   t.pd()
+
+t.st()
+t.pu()
+t.home()
 
 window.exitonclick()
