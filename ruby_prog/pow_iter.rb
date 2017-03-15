196ac233cb020bf0df80459b18516a522d71f929
Mariano Dominguez <mariano_molina@apple.com>
2017-03-15 15:02:48 -0700

    add pow test

---
+++ ruby_prog/pow_iter.rb
@@ -0,0 +1,17 @@
+def pow(a,b)
+  x = a
+  pow = b
+  while pow>1
+   if pow.even?
+      x= x * x
+      pow = pow/2
+   else
+      x = a * x * x
+      pow = pow/2
+   end
+  end
+  x
+end
+
+puts pow 2,16
+puts pow(2,64)-1
