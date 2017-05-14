093cfe88a348a6da7a4eb139b35593c12012c846
marianodominguez <mariano.dm@gmail.com>
2017-05-14 16:04:49 -0700

    add spyrograph sample

---
+++ python/pygame/spyrograph.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import pygame,sys
+import math
+
+w,h=800,600
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+
+red = pygame.color.Color('red')
+yellow = pygame.color.Color('yellow')
+cyan = pygame.color.Color('cyan')
+
+x=0
+y=0
+n=10000
+px,py=-1,-1
+
+def fn(R,t,k,l):
+     x = R*((1-k)*math.cos(t) + l*k*math.cos((1-k)*t/k))
+     y = R*((1-k)*math.sin(t) - l*k*math.sin((1-k)*t/k))
+     return (x,y)
+
+def plot(x,y,c):
+    global px,py
+    point = (int(x+w/2), int(h/2-y) )
+    if (px>=0 and py>=0):
+        pygame.draw.aaline(screen,c,(px,py),point)
+    px,py=point
+
+def spyro(R,r,l, color):
+    global px,py
+    t=0
+    k=r/R
+
+    for i in xrange(0,n):
+        t+=math.radians(1)
+        #x=r*math.sin(t)
+        #y=r*math.cos(t)
+        (x,y) = fn(R,t,k,l)
+        plot(x,y,color)
+    px,py=-1,-1
+
+
+pygame.display.set_caption('Spyrographs !')
+
+spyro(300.0, 53.0, 0.9, red)
+spyro(156.0, 56.0, 0.4, yellow)
+spyro(200.0, 45.0, 0.9, cyan)
+
+while 1:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
+  pygame.display.update()
