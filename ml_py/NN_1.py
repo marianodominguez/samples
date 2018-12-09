859722b6fba9744fa45757791756588d10c8479a
marianodominguez <mariano.dm@gmail.com>
2018-12-08 16:18:48 -0800

    create diabetes NN example

---
+++ ml_py/NN_1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+d = r"/home/mariano/samples/ml_py/diabetes.csv.gz"
+import torch
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+xy = np.loadtxt(d, delimiter=',', dtype=np.float32)
+x_data = Variable(torch.from_numpy(xy[:,0:-1]))
+y_data = Variable(torch.from_numpy(xy[:,[-1]]))
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.l1=torch.nn.Linear(8,6)
+        self.l2=torch.nn.Linear(6,4)
+        self.l3=torch.nn.Linear(4,1)
+        
+        self.sigmoid = torch.nn.Sigmoid()
+        
+    def forward(self,x):
+        out1 = F.relu(self.l1(x))
+        out2 = F.relu(self.l2(out1))
+        y_pred = self.sigmoid(self.l3(out2))
+        return y_pred
+
+model = Model()
+
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(1000):
+    y_pred = model(x_data)
+    
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.data.item())
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    
+
+
+# In[ ]:
+
+
+
+
