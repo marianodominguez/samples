44ba4987a8a7f19ae83de8496ca5443b5cc7dc1a
Mariano Dominguez <mariano.dm@gmail.com>
2020-05-25 17:17:31 -0700

    add c asm examples

---
+++ cprog/sysprog/getreg_rcx.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef unsigned long u64;
+
+    static u64 get_rcx() {
+    __asm__ __volatile__(
+            "push %rcx\n\t" 
+            "movq $5, %rcx\n\t" 
+            "movq %rcx, %rax"); 
+            /* at&t syntax: movq <src_reg>, <dest_reg> */
+    __asm__ __volatile__("pop %rcx");   
+    }
+
+
+int main(void)
+{
+    printf("get value from asm: [RCX] = 0x%lx\n", get_rcx());
+    exit(0);
+}
--- python/turtle/tr_draw.py
+++ python/turtle/tr_draw.py
@@ -12,7 +12,7 @@ t.pensize(2)
 
 def star(n,l):
   angle=720/n
-  for i in range(n):
+  for i in xrange(n):
     t.fd(l)
     t.rt(angle)
 
