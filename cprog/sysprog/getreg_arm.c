3507e5bf1c8a935a89bfaa902f5679198193b247
Mariano Dominguez <mariano.dm@gmail.com>
2020-05-25 17:28:11 -0700

    create arm example

---
+++ cprog/sysprog/getreg_arm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef unsigned long u64;
+
+    static u64 get_rcx() {
+    __asm__ __volatile__(
+            "mov r0, #5\n\t" 
+            "bx lr"); 
+            /* at&t syntax: movq <src_reg>, <dest_reg> */  
+    }
+
+
+int main(void)
+{
+    printf("get value from asm: [RCX] = 0x%lx\n", get_rcx());
+    exit(0);
+}
