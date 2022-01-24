f6fc9f812bdb9248b853071b1b5ac0e1bdbdf24d
Mariano Dominguez <marianod@wepay.com>
2022-01-24 10:54:48 -0800

    add tests rust

---
+++ python/trie.py
@@ -0,0 +1,18 @@
+class Node:
+    def _init_(self):
+        self.root=None
+        self.children={}
+              
+class Tree:
+    def _init_():
+        pass
+    def add_word(word):
+        fn=Node()
+        fn=""
+        for ch in word[1:]:
+            n = Node()
+            n.content=ch
+            
+            
+search_words=["Hello World", "Hello there", "some other word"]
+
---
+++ rust/owner.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x = String::from("hi");
+    let y=&x;
+    println!("{}", x);
+}
