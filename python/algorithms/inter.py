43a0f5d3075d31e926cb7f4b8f6c54f3793b3100
Mariano Dominguez <mariano.dm@gmail.com>
2022-04-02 17:52:59 -0700

    update to python 3

--- python/algorithms/inter.py
+++ python/algorithms/inter.py
@@ -1,7 +1,6 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 l = [ -1, 2, 3, -4, -5, 6, 10, -10]
 
-
 max_val = l[0]
 max_pos = l[0]
 initial=0
@@ -10,10 +9,10 @@ final=0
 for index in range(1,len(l)):
     max_val = max(l[index], max_val+l[index])
     if max_val > max_pos:
-	final = index
+        final = index
         max_pos = max_val
     else: 
-	initial=index
-print "idx=", initial, final
+	    initial=index
+print("idx=", initial, final)
 
 print(max_pos)
--- python/algorithms/list_ex.py
+++ python/algorithms/list_ex.py
@@ -1,21 +1,21 @@
 # list examples
 
-a = [ x for x in xrange(50)]
+a = [ x for x in range(50)]
 
-print '***************** splice ******************'
+print ('***************** splice ******************')
 
-print a[0:10]
+print (a[0:10])
 
-print a[10:]
-print a[:10]
+print (a[10:])
+print (a[:10])
 
-print a[-10:]
-print a[:-10]
+print (a[-10:])
+print (a[:-10])
 
-print '***************** list comprehension ******************'
+print ('***************** list comprehension ******************')
 
-print a
-print [ x*x for x in a]
-print [ x*x*x for x in a if x<10 ]
-print [ x for x in a if x%2 == 1]
-print filter(lambda x : x%2 ==1, a)
+print (a)
+print ([ x*x for x in a])
+print ([ x*x*x for x in a if x<10 ])
+print ([ x for x in a if x%2 == 1])
+print (filter(lambda x : x%2 ==1, a))
