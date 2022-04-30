b311b64d629664e7b7125140a21d54efc61ada97
Mariano Dominguez <mariano.dm@gmail.com>
2022-04-30 14:55:43 -0700

    dumbo puzzle solved

--- python/aoc_code/day11.py
+++ python/aoc_code/day11.py
@@ -1,6 +1,6 @@
 def read_data():
     result=[]
-    f = open("dumbo_tst.txt","r")
+    f = open("dumbo.txt","r")
     for line in f:
         row=[int(x) for x in line.strip()]
         result.append(row)
@@ -15,16 +15,16 @@ def flash(grid,i,j, visited):
         current=stack.pop()
         if current not in visited: 
             visited.append(current)
-        i=current[0]
-        j=current[1]
-        for x in [-1,0,1]:
-            for y in [-1,0,1]:
-                if x+i<n and x+i>=0 and y+j<m and y+j>=0:
-                    if not (x==0 and y==0): 
-                        grid[i+x][j+y]+=1
-                        if grid[i+x][j+y]>9:
-                            if (i+x,j+y) not in visited: 
-                                stack.append((i+x,j+y))
+            i=current[0]
+            j=current[1]
+            for x in [-1,0,1]:
+                for y in [-1,0,1]:
+                    if x+i<n and x+i>=0 and y+j<m and y+j>=0:
+                        if not (x==0 and y==0): 
+                            grid[i+x][j+y]+=1
+                            if grid[i+x][j+y]>9:
+                                if (i+x,j+y) not in visited: 
+                                    stack.append((i+x,j+y))
     return visited
 
 def step(grid):
@@ -48,9 +48,12 @@ def step(grid):
 
 grid = read_data()
 
-nsteps=2
+nsteps=1
 nflashes=0
-for i in range(nsteps):
+n=len(grid[0])
+m=len(grid)
+while sum([sum(row) for row in grid])!=0:
     nflashes+=step(grid)
     print(grid)
-    print(nflashes)
+    print(nsteps)
+    nsteps+=1
