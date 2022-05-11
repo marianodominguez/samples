4b75d47ace45eb31ec151a7155143bce64909b30
Mariano Dominguez <mariano.dm@gmail.com>
2022-05-10 17:14:00 -0700

    day 15 solved

--- python/aoc_code/day15.py
+++ python/aoc_code/day15.py
@@ -1,3 +1,5 @@
+from heapq import heappush,heappop
+
 def read_data():
     result=[]
     f = open("chitons_tst.txt","r")
@@ -6,43 +8,67 @@ def read_data():
         result.append(row)
     return result
 
-def add_neighbors(p,stack,n,m, visited):
-    for x in [-1,0,1]:
-        for y in [-1,0,1]:
-            if x!=0 or y!=0:
-                nx=p[0]+x
-                ny=p[1]+y
-                if nx>=0 and nx<n and ny>=0 and ny<m:
-                    if (nx,ny) and not visited[(nx,ny)]: stack.append( (nx,ny) )
-
-
-def get_path(start, end, cave, path, visited):
-    stack=[start]
-    path=[]
+def expand_map(map):
     result=[]
-    while stack:
-        current=stack.pop()
-        visited[current]=True
-        if current==end:
-            path.append(current)
-            result.append(path)
-            path=[]
-            stack.append((0,0))
-            visited[(0,0)]=False
-            visited[end]=False
-        else:
-            add_neighbors(current,stack, n, m, visited)
-            path.append(current)
+    
+    for maprow in map:
+        nrow=[]
+        for i in range(5):
+            row=[ (x+i-1) % 9 +1 for x in maprow]   
+            nrow.extend(row)
+        result.append(nrow)
+    n=len(result)
+    for i in range(0,4):
+        newrow=[]
+        for j in range(n):
+            newrow=[(x+i) % 9 +1  for x in result[j]]
+            result.append(newrow)
     return result
 
+
+def add_neighbors(p,queue,path,n,m, visited):
+    for c in [(-1,0), (1,0), (0,-1), (0,1)]:
+        x,y=c[0],c[1]
+        nx=p[0]+x
+        ny=p[1]+y
+        if nx>=0 and nx<n and ny>=0 and ny<m:
+            if not visited[p]:
+                newpath=list(path)
+                newpath.append( (nx,ny) )
+                risk = sum( [ cave[p[0]][p[1]] for p in newpath ]  )
+                heappush( queue, (risk,newpath) )
+
+def get_path(start, end, visited):
+    queue=[]
+    heappush(queue, (1, [start]) )
+    path=[]
+    while queue:
+        #print(".", end="")
+        # get the path with min risk
+        path=heappop(queue)[1]
+        current = path[-1]
+        if current==end:
+            return path
+        else:
+            add_neighbors(current,queue,path, n, m, visited)
+        visited[current]=True
+    return path
+
 cave=read_data()
-print(cave)
+cave=expand_map(cave)
+
+i=0
+for row in cave:
+    if i%10==0: print("")
+    i+=1
+    print("".join([ str(x) for x in row]))
+    
+print(len(cave), len(cave[0]))
 start=(0,0)
 m=len(cave)
 n=len(cave[0])
 end=(n-1,m-1)
-path=[]
 visited={(i,j):False for i in range(m) for j in range(n) }
-
-print(get_path(start, end, cave,path, visited))
-
+path=get_path(start, end, visited)
+print(path)
+print( sum( [ cave[c[0]][c[1]] for c in path[1:]] ) )
