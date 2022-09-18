9b46e4dfadc47f18ef8a5b4f570068278d83b02c
Mariano Dominguez <mariano.dm@gmail.com>
2022-09-17 18:02:52 -0700

    fast dragon curve

--- python/pygame/birds.py
+++ python/pygame/birds.py
@@ -3,13 +3,14 @@ import pygame,sys
 import math
 
 w,h=1440,960
+scale=5
 
 pygame.init()
 screen = pygame.display.set_mode((w,h))
 screen.fill(pygame.Color('white'))
 
 
-for k in range(-20000, 20000,4):
+for k in range(-20000, 20000,3):
     a=3*k/45000 + math.sin(17*math.pi/20*math.pow(k/20000,5)) * math.pow(math.cos(41*math.pi*k/20000),6) + (1/3*math.pow(math.cos(41*math.pi*k/20000) ,16) + 1/3*math.pow(math.cos(41*math.pi*k/20000),80) )*math.pow(math.cos(math.pi*k/40000),12)*math.sin(6*math.pi*k/20000)
 
     b=15/30* math.pow(k/20000,4) - math.cos(17*math.pi/20*math.pow(k/20000,5)) * ( 11/10+45/20*math.pow(math.cos(math.pi*k/40000),8)*math.pow(math.cos(3*math.pi*k/40000),6) ) * math.pow(math.cos(41*math.pi*k/20000),6) + 12/20*math.pow(math.cos(3*math.pi*k/200000),10) * math.pow(math.cos(9*math.pi*k/200000),10) * math.pow(math.cos(18*math.pi*k/200000),10)
@@ -21,8 +22,13 @@ for k in range(-20000, 20000,4):
 
     if xp>0 and xp<w and yp>0 and yp<h:
         #screen.set_at((xp,yp),pygame.Color('black'))
-        pygame.draw.circle(screen,pygame.Color('black'),(xp,yp),int(abs(r*200)))
+        pygame.draw.circle(screen,pygame.Color('black'),(xp,yp),int(abs(r*300)), width=1)
         pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+while 1:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
---
+++ python/pygame/dragon_fast.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import pygame,sys
+import math
+
+w,h=1440,960
+
+pygame.init()
+screen = pygame.display.set_mode((w,h))
+screen.fill(pygame.Color('black'))
+s=[0 for i in range(0,240)]
+x=1020
+y=380
+h=0
+v=5
+s[1]=14
+s[2]=1
+p=3
+
+def dragon():
+    global p,n,s,a,x,y,h,v,u,w,t
+    p=p-2
+    n=s[p]
+    a=s[p+1]
+    if n==0:
+        u=x+h
+        w=y-v
+        pygame.draw.line(screen,pygame.Color('yellow'),(x,y),(u,w))
+        pygame.display.update()
+        x=u
+        y=w
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        return
+    s[p]=n-1
+    s[p+1]=-1
+    s[p+2]=a
+    s[p+3]=n-1
+    s[p+4]=1
+    p=p+5
+    dragon()
+    p=p-1
+    a=s[p]
+    t=-a*h
+    h=a*v
+    v=t
+    dragon()
+    return
+        
+dragon()
+while 1:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
+
+
+
+
+
+
