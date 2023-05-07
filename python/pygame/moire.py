eea5db253db98d4178e217a624b6b98d45b15bac
papa_robot <mariano.dm@gmail.com>
2023-05-07 15:16:54 -0700

    fix moire

--- python/pygame/moire.py
+++ python/pygame/moire.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 import pygame,sys
 
+def draw_moire():
+  y=0
+  for x in range(0,w,2):
+    pygame.draw.line(screen,black,(w/2,h/2),(x,0))
+    pygame.draw.line(screen,white,(w/2,h/2),(x+1,0))
+    pygame.draw.line(screen,black,(w/2,h/2),(x,h))
+    pygame.draw.line(screen,white,(w/2,h/2),(x+1,h))
+    if y<h:
+        pygame.draw.line(screen,black,(w/2,h/2),(0,y))
+        pygame.draw.line(screen,white,(w/2,h/2),(0,y+1))
+        pygame.draw.line(screen,black,(w/2,h/2),(w,y))
+        pygame.draw.line(screen,white,(w/2,h/2),(w,y+1))
+    y+=2
+    pygame.display.update()
+    pygame.event.get();
+
+
 w,h=1440,960
 
 black = pygame.Color('black')
 white = pygame.Color('white')
 
 pygame.init()
-screen = pygame.display.set_mode((w,h))
+screen = pygame.display.set_mode((w,h), pygame.RESIZABLE)
 screen.fill(black)
-y=0
-for x in range(0,w,2):
-        pygame.draw.line(screen,black,(w/2,h/2),(x,0))
-        pygame.draw.line(screen,white,(w/2,h/2),(x+1,0))
-        pygame.draw.line(screen,black,(w/2,h/2),(x,h))
-        pygame.draw.line(screen,white,(w/2,h/2),(x+1,h))
-        if y<h:
-            pygame.draw.line(screen,black,(w/2,h/2),(0,y))
-            pygame.draw.line(screen,white,(w/2,h/2),(0,y+1))
-            pygame.draw.line(screen,black,(w/2,h/2),(w,y))
-            pygame.draw.line(screen,white,(w/2,h/2),(w,y+1))
-        y+=2
-        pygame.display.update()
-        pygame.event.get();
+draw_moire()
 
 while 1:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
         sys.exit()
+    if event.type == pygame.VIDEORESIZE:
+        w,h = event.w,event.h
+        screen = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
+        draw_moire()
--- python/pygame/moire_sin.py
+++ python/pygame/moire_sin.py
@@ -4,23 +4,30 @@ import math
 
 w,h=1440,960
 
+def draw():
+  y=0
+  x=0
+  for i in range(0,w,2):
+        y=h/3*math.sin(i/(40*math.pi)) + h/2
+        x=i
+        pygame.draw.line(screen,white,(w/2,0),(x,y))
+        #pygame.draw.line(screen,white,(x,y),(w,h))
+        pygame.display.update()
+        pygame.event.get()
+
 black = pygame.Color('black')
 white = pygame.Color('white')
 
 pygame.init()
-screen = pygame.display.set_mode((w,h))
+screen = pygame.display.set_mode((w,h),pygame.RESIZABLE)
 screen.fill(black)
-y=0
-x=0
-for i in range(0,w,2):
-        y=h/2*math.sin(i/(40*math.pi)) + h/2
-        x=i
-        pygame.draw.line(screen,white,(0,0),(x,y))
-        pygame.draw.line(screen,white,(x,y),(w,h))
-        pygame.display.update()
-        pygame.event.get()
+draw()
 
 while 1:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
         sys.exit()
+    if event.type == pygame.VIDEORESIZE:
+        w,h = event.w,event.h
+        screen = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
+        draw()
