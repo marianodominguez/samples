e1f2e0493a916f8a697039baaeefe209a24b93d3
Mariano Dominguez <mariano.dm@gmail.com>
2024-03-10 18:48:23 -0700

    add big hat

---
+++ python/pygame/hat.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import pygame,sys
+import math
+
+def plot(xx,yy,zz,p,q):
+   x1=xx+zz+p
+   y1=-yy+zz+q
+   if y1==0: return
+   screen.set_at((int(x1),int(y1)),pygame.Color('white'))
+
+def draw():
+  y=0
+  p=int(w/2)
+  q=int(h/2)
+  xp=144*p/160
+  xr=1.5*math.pi
+  yp=56*q/100
+  yr=1
+  zp=64*q/200
+  xf=xr/xp
+  yf=yp/yr
+  zf=xr/zp
+  for z in range(-q,q-1):
+    if z<-zp or z>zp: continue
+    zt=z*xp/zp
+    zz=z
+    xl=int(0.5+math.sqrt(xp*xp-zt*zt))
+    for x in range(-xl,xl):
+       xt=math.sqrt(x*x+zt*zt)*xf
+       xx=x
+       yy=(math.sin(xt)+0.4*math.sin(3*xt))*yf
+       plot(xx,yy,zz,p,q)
+    pygame.display.update()
+    pygame.event.get()
+
+w,h=1440,960
+
+black = pygame.Color('black')
+white = pygame.Color('white')
+
+pygame.init()
+screen = pygame.display.set_mode((w,h), pygame.RESIZABLE)
+screen.fill(black)
+draw()
+
+while 1:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
+    if event.type == pygame.VIDEORESIZE:
+        w,h = event.w,event.h
+        screen.fill(black)
+        draw()
