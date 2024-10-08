5f87965cf5de2318c8b6f8b471b44d3f1bf25118
Mariano <mariano.dm@gmail.com>
2024-10-08 08:56:58 -0700

    palindrome string function

--- rust/lychrel/src/main.rs
+++ rust/lychrel/src/main.rs
@@ -1,10 +1,16 @@
 fn ispalindrome(n:u64) -> bool {
     let num_as_string = n.to_string();
-    let mut inv = n.to_string();
-    inv.chars().rev().collect();
+    let mut inv:String = n.to_string();
+    inv = reverse_string(&inv);
+    println!("{},{}", inv, num_as_string);
     inv == num_as_string
 }
 
+fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+  }
+
 fn main() {
-    println!("{}", ispalindrome(123321))
+    println!("{}", ispalindrome(123321));
+    println!("{}", ispalindrome(123211));
 }
