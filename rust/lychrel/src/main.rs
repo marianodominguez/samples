000f2a05302b8efceb32485da19ba751266d3330
Mariano Dominguez <marianod@wepay.com>
2024-10-08 12:59:19 -0700

    simple loop lychrel

--- rust/lychrel/src/main.rs
+++ rust/lychrel/src/main.rs
@@ -1,8 +1,13 @@
-fn ispalindrome(n:u64) -> bool {
+extern crate num;
+use num_bigint::BigUint;
+use num::bigint::{ToBigUint};
+const MAX_ITER:u32=10000;
+
+fn ispalindrome(n:&BigUint) -> bool {
     let num_as_string = n.to_string();
     let mut inv:String = n.to_string();
     inv = reverse_string(&inv);
-    println!("{},{}", inv, num_as_string);
+    //println!("{},{}", inv, num_as_string);
     inv == num_as_string
 }
 
@@ -11,6 +16,22 @@ fn reverse_string(s: &str) -> String {
   }
 
 fn main() {
-    println!("{}", ispalindrome(123321));
-    println!("{}", ispalindrome(123211));
+    for i in 100..10000 {
+        let mut n:BigUint = i.to_biguint().unwrap();
+        let mut iter=0;
+        
+        while !ispalindrome(&n) && iter<MAX_ITER{
+            let mut inv:String = n.to_string();
+            inv = reverse_string(&inv);
+            let inv_int:BigUint=inv.parse::<BigUint>().unwrap();
+            n=n+inv_int;
+            iter+=1;
+        }
+        if iter>= MAX_ITER {
+            println!("{i} possible Lychrel, {iter}")
+        }
+        else {
+            println!("{i} is palindrome {iter}, {n}")
+        }
+    }
 }
