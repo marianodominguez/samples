bcc8cf71fd54270b2702b81c4ac758b3e3115c41
Mariano <mariano.dm@gmail.com>
2025-02-12 13:39:28 -0800

    Add go sdl sample code

--- golang/picture.go
+++ golang/picture.go
@@ -1,24 +1,21 @@
 package main
 
-import "code.google.com/p/go-tour/pic"
-
 func Pic(dx, dy int) [][]uint8 {
-    image := make([][] uint8, dx)
-    for i:= range image {
-        col := make([] uint8, dy)
-        image[i] = col
-    }
-    
-    for x:=0; x<dx ; x++ {
-        for y:=0; y<dy; y++ {
-        image[x][y] = uint8((x*y))
-        }
-    }
-    
-    return image 
+	image := make([][]uint8, dx)
+	for i := range image {
+		col := make([]uint8, dy)
+		image[i] = col
+	}
+
+	for x := 0; x < dx; x++ {
+		for y := 0; y < dy; y++ {
+			image[x][y] = uint8((x * y))
+		}
+	}
+
+	return image
 }
 
 func main() {
-    pic.Show(Pic)
+	pic.Show(Pic)
 }
-
--- golang/wordcount.go
+++ golang/wordcount.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-    "code.google.com/p/go-tour/wc"
-    "strings"
+	"strings"
 )
 
 func WordCount(s string) map[string]int {
 
-    mapcount := make(map[string]int)
+	mapcount := make(map[string]int)
 
-    for _,word:=range strings.Fields(s) {
-        times, present := mapcount[word]
-        if present {
-            mapcount[word] = times + 1
-        } else {
-            mapcount[word] = 1
-        }
-    }
+	for _, word := range strings.Fields(s) {
+		times, present := mapcount[word]
+		if present {
+			mapcount[word] = times + 1
+		} else {
+			mapcount[word] = 1
+		}
+	}
 
-    return mapcount
+	return mapcount
 }
 
 func main() {
-    wc.Test(WordCount)
+	wc.Test(WordCount)
 }
