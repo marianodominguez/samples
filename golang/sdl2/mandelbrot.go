217bbe1b4727f71c449d6d81e3ffe223d8cee843
Mariano DM <mariano.dm@gmail.com>
2025-03-23 12:35:08 -0700

    use orbit instead of distance

--- golang/sdl2/mandelbrot.go
+++ golang/sdl2/mandelbrot.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"math/cmplx"
 
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
 )
 
 const MAX_ITERATIONS = 600
@@ -26,19 +28,50 @@ func mouseEvent() (bool, float64, float64, bool) {
 	return true, 0, 0, false
 }
 
-func orbit(x float64, y float64) float64 {
+func orbit(x float64, y float64) int {
 	var d float64
 	var z = complex(0, 0)
 	var c = complex(x, y)
-	for i := 0; i < MAX_ITERATIONS && d < 4.0; i++ {
+	var i int
+	for i = 0; i < MAX_ITERATIONS && d <= 5.0; i++ {
 		z = z*z + c
 		d = cmplx.Abs(z)
 	}
-	return d
+	return 10 - (i*10)/MAX_ITERATIONS
 }
 
-func gcolor(o float64) color.Color {
-	return color.RGBA{0, (uint8)(255 * o / 10), (uint8)(255 * o / 10), 255}
+func gcolor(o int) color.Color {
+	return gcolorGradient(o)
+}
+
+func gcolorSmooth(o int) color.Color {
+	c := uint8(255 * o / 10) // Scale orbit value to hue (0-255)
+	return color.RGBA{R: 0, G: c, B: c, A: 255}
+}
+
+func gcolorGradient(o int) color.Color {
+	// Map the orbit value to a color gradient
+	if o < 1 {
+		return color.RGBA{R: 66, G: 30, B: 15, A: 255} // Dark brown
+	} else if o < 2 {
+		return color.RGBA{R: 25, G: 7, B: 26, A: 255} // Purple
+	} else if o < 3 {
+		return color.RGBA{R: 9, G: 1, B: 47, A: 255} // Dark blue
+	} else if o < 4 {
+		return color.RGBA{R: 4, G: 4, B: 73, A: 255} // Blue
+	} else if o < 5 {
+		return color.RGBA{R: 0, G: 7, B: 100, A: 255} // Light blue
+	} else if o < 6 {
+		return color.RGBA{R: 12, G: 44, B: 138, A: 255} // Cyan
+	} else if o < 7 {
+		return color.RGBA{R: 24, G: 82, B: 177, A: 255} // Light cyan
+	} else if o < 8 {
+		return color.RGBA{R: 57, G: 125, B: 209, A: 255} // Sky blue
+	} else if o < 9 {
+		return color.RGBA{R: 134, G: 181, B: 229, A: 255} // Pale blue
+	} else {
+		return color.RGBA{R: 211, G: 236, B: 248, A: 255} // White
+	}
 }
 
 func renderMandelbrot(surface *sdl.Surface, xmin, xmax, ymin, ymax float64) bool {
@@ -59,11 +92,26 @@ func renderMandelbrot(surface *sdl.Surface, xmin, xmax, ymin, ymax float64) bool
 	return true
 }
 
+func renderText(surface *sdl.Surface, font *ttf.Font, text string, x, y int32) {
+	color := sdl.Color{R: 255, G: 255, B: 255, A: 255} // White color
+	textSurface, err := font.RenderUTF8Solid(text, color)
+	if err != nil {
+		panic(err)
+	}
+	textRect := sdl.Rect{X: x, Y: y, W: textSurface.W, H: textSurface.H}
+	surface.Blit(&textRect, textSurface, nil)
+}
+
 func mandelbrot() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
+
+	if err := ttf.Init(); err != nil {
+		panic(err)
+	}
+	defer ttf.Quit()
 
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, sdl.WINDOW_SHOWN)
 	if err != nil {
@@ -76,6 +124,8 @@ func mandelbrot() {
 	if err != nil {
 		panic(err)
 	}
+	font, err := ttf.OpenFont("/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf", 50) // Replace with the path to your font file
+
 	surface.FillRect(&rect, 0)
 
 	sdl.Delay(100)
@@ -94,6 +144,8 @@ func mandelbrot() {
 	ymax := 1.5
 	clicked := false
 	renderMandelbrot(surface, xmin, xmax, ymin, ymax)
+	renderText(surface, font, fmt.Sprintf("Click to begin"), 10, 10)
+
 	window.UpdateSurface()
 
 	for clicked == false {
@@ -133,5 +185,4 @@ func mandelbrot() {
 			break
 		}
 	}
-
 }
